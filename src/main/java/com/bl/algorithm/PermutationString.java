55efe431f65359b40336c23a0bc3724be5521e33
unknown <altamashbagwan1@gmail.com>
2021-11-12 00:58:06 +0530

    Added: Functioning of Permutation String Find

---
+++ src/main/java/com/bl/algorithm/PermutationString.java
@@ -0,0 +1,36 @@
+package com.bl.algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermutationString {
+    static Set<String> set = new HashSet<>();
+
+    public static void permutation(String array, int l, int r) {
+        if (l == r) {
+            if (set.contains(array))
+                return;
+            set.add(array);
+            System.out.println("the " + array);
+            return;
+        }
+        for (int i = l; i <= r; i++) {
+            array = charInterchange(array, l, i);
+            permutation(array, l + 1, r);
+            array = charInterchange(array, l, i);
+        }
+    }
+
+    static String charInterchange(String s, int a, int b) {
+        char arry2[] = s.toCharArray();
+        char temp = arry2[a];
+        arry2[a] = arry2[b];
+        arry2[b] = temp;
+        return String.valueOf(arry2);
+    }
+
+    public static void main(String[] args) {
+        permutation("abcd", 0, 3);
+
+    }
+}
